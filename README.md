66c70b6febc17c74a8afd83564fd5ce12bd78c97
Carl Bäckström <genzorg@gmail.com>
2020-10-03 22:39:07 +0200

    Using Types
    https://www.udemy.com/course/understanding-typescript/learn/lecture/16888054

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Udemy Typescript
+
+## Core Types
+
+![Core Types](https://cdn.jsdelivr.net/gh/carlba/assets@master/7AjUWA-9lbHqr.png)
--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
-console.log('Your code goes here...');
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+// Javascript will concatinate numbers and strings into strings.
+const number1 = '5';
+const number2 = 2.8;
+
+console.log(add(number1, number2));
