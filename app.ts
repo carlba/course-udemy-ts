c0292f9f39199dc61e6818cd14498d1bb6f54459
Carl Bäckström <genzorg@gmail.com>
2020-10-03 23:22:38 +0200

    TypeScript Types vs JavaScript Types
    https://www.udemy.com/course/understanding-typescript/learn/lecture/16888058

--- app.ts
+++ app.ts
@@ -1,9 +1,12 @@
 function add(n1: number, n2: number) {
+  if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    throw new Error('Incorrect Type');
+  }
   return n1 + n2;
 }
 
 // Javascript will concatinate numbers and strings into strings.
-const number1 = '5';
+const number1 = 5;
 const number2 = 2.8;
-
-console.log(add(number1, number2));
+const result = add(number1, number2);
+console.log(result);
