4badd9b8a8eaca5804bd2106cdc32046890f5a53
Carl Bäckström <genzorg@gmail.com>
2020-10-04 19:55:40 +0200

    Arrays TypesNested
    https://www.udemy.com/course/understanding-typescript/learn/lecture/16888066#notes

--- app.ts
+++ app.ts
@@ -1,3 +1,13 @@
-const person = { name: 'Carl', age: 37 };
+const person = { name: 'Carl', age: 37, hobbies: ['sports', 'cooking'] };
+
+let favoriteActivities: string[];
+favoriteActivities = ['sports'];
+
+// Typescript infers that hobbies is a list of strings and can therefore thrugh the
+// IDE provide autocompletion for the invidual items when looping through the array.
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  //   console.log(hobby.map) // Map does not exist on a string
+}
 
 console.log(person.name);
