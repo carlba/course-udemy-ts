62d1cf2c273a5acf8b1db58b52dba68919918445
Carl Bäckström <genzorg@gmail.com>
2020-10-04 16:15:54 +0200

    Working with Numbers, Strings & Booleans
    https://www.udemy.com/course/understanding-typescript/learn/lecture/16888062

--- app.ts
+++ app.ts
@@ -1,12 +1,20 @@
-function add(n1: number, n2: number) {
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
   if (typeof n1 !== 'number' || typeof n2 !== 'number') {
     throw new Error('Incorrect Type');
   }
-  return n1 + n2;
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return n1 + n2;
+  }
 }
 
 // Javascript will concatinate numbers and strings into strings.
+// Javascript and Typescript only has the number type which covers all types of numbers.
 const number1 = 5;
 const number2 = 2.8;
-const result = add(number1, number2);
-console.log(result);
+const printResult = true;
+const resultPhrase = 'Result is: ';
+
+add(number1, number2, printResult, resultPhrase);
