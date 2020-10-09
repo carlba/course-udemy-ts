fa6bcb23e4cc0fd6fb2c3eab825b9654c1c661c4
Carl Bäckström <genzorg@gmail.com>
2020-10-09 10:13:59 +0200

    Another Generic Function
    https://www.udemy.com/course/understanding-typescript/learn/lecture/16894056

--- src/generics.ts
+++ src/generics.ts
@@ -18,3 +18,19 @@ console.log(merge({ name: 'Carl ' }, { age: 22 }));
 
 // Not allowed with the type constraint
 //console.log(merge({ name: 'Carl ' }, 30));
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value.';
+  if (element.length === 1) {
+    descriptionText = `Got 1 element `;
+  } else if (element.length > 1) {
+    descriptionText = `Got ${element.length} elements.`;
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe(['sports', 'cooking']));
