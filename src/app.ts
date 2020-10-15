7b64d2ab35fb22595c33051472d7b3593274e6cd
Carl Bäckström <genzorg@gmail.com>
2020-10-15 14:20:26 +0200

    Fetching User Input
    https://www.udemy.com/course/understanding-typescript/learn/lecture/16935782)

--- src/app.ts
+++ src/app.ts
@@ -64,10 +64,46 @@ class ProjectInput {
     this.hostElement.insertAdjacentElement('afterbegin', this.element);
   }
 
+  /**
+   * The `void` return type allows one of the branches of the function to not return a value.
+   *
+   *
+   * [Fetching User Input](https://www.udemy.com/course/understanding-typescript/learn/lecture/16935782)
+   *
+   * @returns A tuple with three elements `string`, `string` and number. Declared in
+   *          ts using the `[string, string, number]` syntax.
+   */
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert('Invalid input, please try again!');
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.peopleInputElement.value = '';
+  }
+
   @AutoBind
   private submitHandler(event: Event): void {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput;
+      console.log(title, description, people);
+      this.clearInputs();
+    }
   }
 
   private configure(): void {
